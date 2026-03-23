96b584643521d5d23d5276e5a1593d6fbdef2988
Kaique Cerri <144399326+kaiquecerri@users.noreply.github.com>
2026-03-23 09:55:05 -0300

    Add Main class to calculate result based on input

---
+++ 2413.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        int t = input.nextInt();
+        int result = t * 4;
+        System.out.println(result);
+ 
+    }
+ 
+}
