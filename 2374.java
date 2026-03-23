37b176f3251870d097ebcbef2d5678911d60c401
Kaique Cerri <144399326+kaiquecerri@users.noreply.github.com>
2026-03-23 09:58:32 -0300

    Implement subtraction of two integers

---
+++ 2374.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        int v1 = input.nextInt();
+        int v2 = input.nextInt();
+        
+        int res = v1 - v2;
+        
+        System.out.println(res);
+    
+ 
+    }
+ 
+}
